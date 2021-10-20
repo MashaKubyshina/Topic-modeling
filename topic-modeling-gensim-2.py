315c654929d358cde68f52b056bb5aea405fda49
Masha Kubyshina <mksalvado@gmail.com>
2021-10-20 06:53:45 -0700

    updated 10/20/2021

--- topic-modeling-gensim-2.py
+++ topic-modeling-gensim-2.py
@@ -1,18 +1,10 @@
-# documentation https://radimrehurek.com/gensim/auto_examples/core/run_core_concepts.html#sphx-glr-auto-examples-core-run-core-concepts-py
+# documentation https://radimrehurek.com/gensim/auto_examples/core/run_corpora_and_vector_spaces.html
 
-import pprint
-import gensim
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# import the document
-document = "Human machine interface for lab abc computer applications"
-
-# import corpus, a collection of documents
-# corpus will be used for training the model
-# the model is trained to lok for common themes and topics
-# gensim is focused on unsupervised models (documents are not tagged by a human)
-# corpus is also used to find similarity queries (semantic similarities and clusters)
-
-text_corpus = [
+# create a small corpus
+documents = [
     "Human machine interface for lab abc computer applications",
     "A survey of user opinion of computer system response time",
     "The EPS user interface management system",
@@ -24,84 +16,87 @@ text_corpus = [
     "Graph minors A survey",
 ]
 
-# Next step is to pre-process the corpus (remove common words, tokenize)
-# Create a set of frequent words
+# next we will tokenize the text, remove stopwords, and words that appear once
 
-stoplist = set('for a of the and to in'.split(' '))
-
-# Lowercase each document, split it by white space and filter out stopwords
-texts = [[word for word in document.lower().split() if word not in stoplist]
-         for document in text_corpus]
-
-# Count word frequencies
+from pprint import pprint  # pretty-printer
 from collections import defaultdict
+
+# remove common words and tokenize
+stoplist = set('for a of the and to in'.split())
+texts = [
+    [word for word in document.lower().split() if word not in stoplist]
+    for document in documents
+]
+
+# remove words that appear only once
 frequency = defaultdict(int)
 for text in texts:
     for token in text:
         frequency[token] += 1
 
-# Only keep words that appear more than once
-processed_corpus = [[token for token in text if frequency[token] > 1] for text in texts]
-pprint.pprint(processed_corpus)
+texts = [
+    [token for token in text if frequency[token] > 1]
+    for text in texts
+]
 
-# next we need to associate each word in the corpus with a unique integer ID
-# for this we use dictionary class
+pprint(texts)
+
+# next we will convert words to vectors using document representation called "bag-of-words"
+# each document is represented by one vector
+# each vector represents question-answer pair
 
 from gensim import corpora
-
-dictionary = corpora.Dictionary(processed_corpus)
+dictionary = corpora.Dictionary(texts)
+dictionary.save('/tmp/deerwester.dict')  # store the dictionary, for future reference
 print(dictionary)
 
-# next we are going to convert words to vectors
-# and documents as vectors of features (a phrase can be single feature)
-# single feature can be thought as a question-answer pair
-# we know it as "dense vector" because it contains answers to questions, including zero values
-# gensim omits zero values, it is known as sparse vector or bag-of-words
-# if vectors of two documents are similar this means the documents are similar as well
+# mapping between questions and IDs is called dictionary
+print(dictionary.token2id)
 
-# we use dictionary to turn tokenized documents into 12-dimensional vectors
-# we have 12 unique words
-
-pprint.pprint(dictionary.token2id)
-
-# test the vectorization on a new document that is not in the corpus
+# we can also convert tokenized documents to vectors
 new_doc = "Human computer interaction"
 new_vec = dictionary.doc2bow(new_doc.lower().split())
-print(new_vec)
+print(new_vec)  # the word "interaction" does not appear in the dictionary and is ignored
 
-# next we convert our entire original corpus into a list of vectors
-bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
-pprint.pprint(bow_corpus)
+# result of the code above is [(0, 1), (1, 1)]
+# where (0,1) is for human, and (1,1) is for computer (both words appear in the dictionary)
+# interaction didn't appear in the text, thus not part of the dictionary
 
-# now we will move to training the model on our corpus
-from gensim import models
+corpus = [dictionary.doc2bow(text) for text in texts]
+corpora.MmCorpus.serialize('/tmp/deerwester.mm', corpus)  # store to disk, for later use
+print(corpus)
 
-# we will use Tfidf model from gensim
-# tf-idf transforms vectors from the bag-of-words representation
-# to a vector space where frequency counts are weighted according to
-# rarity of each word in the corpus
+from smart_open import open  # for transparently opening remote files
 
-# train the model
-tfidf = models.TfidfModel(bow_corpus)
+# we are going to open document that is stored online
 
-# transform the "system minors" string
-words = "system minors".lower().split()
-print(tfidf[dictionary.doc2bow(words)])
-# in the output we will see that ID corresponding to "system"
-# (occurred 4 times) was weighted down
+class MyCorpus:
+    def __iter__(self):
+        for line in open('https://radimrehurek.com/mycorpus.txt'):
+            # assume there's one document per line, tokens separated by whitespace
+            yield dictionary.doc2bow(line.lower().split())
 
-from gensim import similarities
+# Gensim accepts any corpus (no need for it to be pandas, list or array)
+# the document can we created on the fly
+ # doesn't load the corpus into memory!
+corpus_memory_friendly = MyCorpus()
+print(corpus_memory_friendly)
 
-# now we can transform the whole corpus
-index = similarities.SparseMatrixSimilarity(tfidf[bow_corpus], num_features=12)
+# load one vector into memory at a time
+for vector in corpus_memory_friendly:
+    print(vector)
 
-# and query the similarity of query document against every document of the corpus
-query_document = 'system engineering'.split()
-query_bow = dictionary.doc2bow(query_document)
-sims = index[tfidf[query_bow]]
-print(list(enumerate(sims)))
 
-# the document with highest similarity score will be placed at the top
-
-for document_number, score in sorted(enumerate(sims), key=lambda x: x[1], reverse=True):
-    print(document_number, score)
+# construct dictionary without loading text into memory
+# collect statistics about all tokens
+dictionary = corpora.Dictionary(line.lower().split() for line in open('https://radimrehurek.com/mycorpus.txt'))
+# remove stop words and words that appear only once
+stop_ids = [
+    dictionary.token2id[stopword]
+    for stopword in stoplist
+    if stopword in dictionary.token2id
+]
+once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq == 1]
+dictionary.filter_tokens(stop_ids + once_ids)  # remove stop words and words that appear only once
+dictionary.compactify()  # remove gaps in id sequence after words that were removed
+print(dictionary)
